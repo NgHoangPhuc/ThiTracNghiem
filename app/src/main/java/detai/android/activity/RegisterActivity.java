958b5bf092373dcf5b0a9146128da6dd24074d09
Hoàng Phúc <hoangphucd14cn05@gmail.com>
2018-05-16 12:30:25 +0700

    Revert "hoanchinh"
    This reverts commit 65385ef7951925ab8b651c3b1e10b7b87eab0e48.

---
+++ app/src/main/java/detai/android/activity/RegisterActivity.java
@@ -0,0 +1,235 @@
+package detai.android.activity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import detai.android.Object.message.Message;
+import detai.android.thitracnghiem.R;
+
+/**
+ * Created by hoang on 06-May-18.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText editTextUsername;
+    EditText editTextPassword;
+    EditText editTextRePassword;
+    EditText editTextName;
+    EditText editTextSearch;
+
+    Button btnRegister;
+    Button btnSearch;
+
+    RadioButton rbGiaoVien;
+    RadioButton rbHocSinh;
+
+    Spinner spinner;
+
+    LinearLayout layoutHocsinh;
+
+    ArrayList<String> danhsachlop = new ArrayList<>();
+
+    boolean haditem;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        addControls();
+        addEvents();
+        khoiTaoGiaTriBanDau();
+
+    }
+
+    private void khoiTaoGiaTriBanDau() {
+
+    }
+
+    private void addEvents() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = editTextUsername.getText().toString();
+                final String password = editTextPassword.getText().toString();
+                String repassword = editTextRePassword.getText().toString();
+                final String idgiaovien = editTextSearch.getText().toString();
+                final String lop;
+                final String ten = editTextName.getText().toString();
+                if(spinner.getCount() > 0)
+                    lop = spinner.getSelectedItem().toString();
+                else
+                    lop = "";
+                String tucach = "";
+                if(rbGiaoVien.isChecked())
+                    tucach = "GiaoVien";
+                else
+                    tucach = "HocSinh";
+                if(username.equals(""))
+                    Message.showDialog(RegisterActivity.this,"Username chưa được nhập");
+                else if(password.equals(""))
+                    Message.showDialog(RegisterActivity.this,"Mật khẩu chưa được nhập");
+                else if(!password.equals(repassword))
+                    Message.showDialog(RegisterActivity.this,"Mật khẩu 1 và mật khẩu 2 chưa giống nhau");
+                else if(ten.equals(""))
+                    Message.showDialog(RegisterActivity.this,"Tên chưa được nhập");
+                else if(idgiaovien.equals(""))
+                    Message.showDialog(RegisterActivity.this,"Id giáo viên chưa được nhập");
+                else if(lop.equals(""))
+                    Message.showDialog(RegisterActivity.this,"Giáo viên này hiện chưa mở lớp nào.");
+                else {
+                    final DatabaseReference user = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/").child("DanhSach" + tucach).child(username);
+                    Query account = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/").child("DanhSach"+tucach).child(username);
+                    final String finalTucach = tucach;
+                    account.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Iterator<DataSnapshot> items = dataSnapshot.getChildren().iterator();
+                            haditem = false;
+                            if (!items.hasNext()) {
+                                if (finalTucach.equals("HocSinh")) {
+                                    user.child("GiaoVien").setValue(idgiaovien);
+                                    user.child("Lop").setValue(lop);
+                                    user.child("Ten").setValue(ten);
+                                    FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/")
+                                            .child("DanhSachGiaoVien").child(idgiaovien).child(lop).child(username).setValue("-1");
+                                }
+                                showDialogYN(RegisterActivity.this, "Đăng ký thành công, bạn có muốn trở về trang đăng nhập?");
+                            }
+                            else
+                                haditem = true;
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Message.showDialog(RegisterActivity.this, "Không thể lấy dữ liệu từ máy chủ, vui lòng kiểm tra lại mạng");
+                        }
+                    });
+                    if (haditem == true)
+                        Message.showDialog(RegisterActivity.this, "Đã tồn tại id này.");
+                }
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                danhsachlop.removeAll(danhsachlop);
+                spinner.setAdapter(new ArrayAdapter<String>(RegisterActivity.this,R.layout.support_simple_spinner_dropdown_item,danhsachlop));
+                String idgiaovien = editTextSearch.getText().toString();
+                if(idgiaovien.equals("")){
+                    Message.showDialog(RegisterActivity.this,"ID giáo viên chưa được nhập.");
+                }
+                else {
+                    Query account = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/").child("DanhSachGiaoVien").child(idgiaovien).child("DanhSachLop");
+                    account.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Iterator<DataSnapshot> items = dataSnapshot.getChildren().iterator();
+                            if (!items.hasNext()) {
+                                Message.showDialog(RegisterActivity.this, "Không tìm thấy giáo viên.");
+                                return;
+                            }
+                            while (items.hasNext()) {
+                                DataSnapshot item = items.next();
+                                if (item == null) {
+                                    return;
+                                }
+                                danhsachlop.add(item.getKey());
+                            }
+                            ArrayAdapter arrayAdapter = new ArrayAdapter(RegisterActivity.this,R.layout.support_simple_spinner_dropdown_item,danhsachlop);
+                            spinner.setAdapter(arrayAdapter);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Message.showDialog(RegisterActivity.this, "Không thể lấy dữ liệu từ máy chủ, vui lòng kiểm tra lại mạng");
+                        }
+                    });
+                }
+            }
+        });
+        editTextSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                danhsachlop.removeAll(danhsachlop);
+                spinner.setAdapter(new ArrayAdapter<String>(RegisterActivity.this,R.layout.support_simple_spinner_dropdown_item,danhsachlop));
+            }
+        });
+        rbGiaoVien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutHocsinh.setVisibility(View.INVISIBLE);
+            }
+        });
+        rbHocSinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutHocsinh.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void addControls() {
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextRePassword = findViewById(R.id.editTextRePassword);
+        editTextSearch = findViewById(R.id.editTextSearch);
+        editTextName = findViewById(R.id.editTextName);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSearch = findViewById(R.id.btnSearch);
+        rbGiaoVien = findViewById(R.id.rbGiaoVien);
+        rbHocSinh = findViewById(R.id.rbHocSinh);
+        spinner = findViewById(R.id.spinner);
+        layoutHocsinh = findViewById(R.id.layoutHocsinh);
+    }
+
+    void showDialogYN(Context context, String message){
+        AlertDialog.Builder alertbuild = new AlertDialog.Builder(context);
+        alertbuild.setMessage(message);
+        alertbuild.setCancelable(true);
+        alertbuild.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        dialog.cancel();
+                    }
+                });
+
+        alertbuild.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        editTextUsername.setText("");
+                        editTextPassword.setText("");
+                        editTextRePassword.setText("");
+                        editTextSearch.setText("");
+                        editTextSearch.callOnClick();
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = alertbuild.create();
+        alert11.show();
+    }
+}
---
+++ app/src/main/java/detai/android/activity/giaovien/XemDiemActivity.java
@@ -0,0 +1,185 @@
+package detai.android.activity.giaovien;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import detai.android.Adapter.UserInfoAdapter;
+import detai.android.Object.UserInfo;
+import detai.android.Object.session.SessionManager;
+import detai.android.thitracnghiem.R;
+
+/**
+ * Created by hoang on 06-May-18.
+ */
+
+public class XemDiemActivity extends AppCompatActivity {
+    ListView listView;
+    Spinner spinnerDanhSachLop;
+    Spinner spinnerDanhSachDe;
+
+    ArrayList<String> listde;
+    ArrayList<String> listlop;
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giaovien_xemdiem);
+
+        addControls();
+        addEvents();
+        khoiTaoGiaTriBanDau();
+    }
+
+    private void khoiTaoGiaTriBanDau() {
+        final SessionManager sessionManager = new SessionManager(XemDiemActivity.this);
+        final Query giaovien = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/")
+                .child("DanhSachGiaoVien").child(sessionManager.getUsername());
+        giaovien.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> items = dataSnapshot.child("DanhSachLop").getChildren().iterator();
+                Iterator<DataSnapshot> items2 = dataSnapshot.child("DanhSachDe").getChildren().iterator();
+                while (items.hasNext()) {
+                    DataSnapshot item = items.next();
+                    listlop.add(item.getKey());
+                    spinnerDanhSachLop.setAdapter(new ArrayAdapter<String>(XemDiemActivity.this, R.layout.support_simple_spinner_dropdown_item, listlop));
+                }
+                while (items2.hasNext()) {
+                    DataSnapshot item = items2.next();
+                    listde.add(item.getKey());
+                    spinnerDanhSachDe.setAdapter(new ArrayAdapter<String>(XemDiemActivity.this, R.layout.support_simple_spinner_dropdown_item, listde));
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        spinnerDanhSachLop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                listde.removeAll(listde);
+                spinnerDanhSachDe.setAdapter(new ArrayAdapter<String>(XemDiemActivity.this, R.layout.support_simple_spinner_dropdown_item, listde));
+                giaovien.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Iterator<DataSnapshot> items2 = dataSnapshot.child("DanhSachDe").getChildren().iterator();
+                        while (items2.hasNext()) {
+                            DataSnapshot item = items2.next();
+                            listde.add(item.getKey());
+                            spinnerDanhSachDe.setAdapter(new ArrayAdapter<String>(XemDiemActivity.this, R.layout.support_simple_spinner_dropdown_item, listde));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinnerDanhSachDe.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Query database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tracnghiem-data001.firebaseio.com/");
+                database.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Iterator<DataSnapshot> hstronglop = dataSnapshot.child("DanhSachGiaoVien").child(sessionManager.getUsername())
+                                .child("DanhSachLop").child(spinnerDanhSachLop.getSelectedItem().toString()).getChildren().iterator();
+                        ArrayList<UserInfo> lisths = new ArrayList<>();
+                        while (hstronglop.hasNext()) {
+                            DataSnapshot hs = hstronglop.next();
+                            if (hs.getKey().equals("De"))
+                                continue;
+//                            Message.showDialog(XemDiemActivity.this, "Hoc sinh trong lop: " + hs.toString());
+
+                            Iterator<DataSnapshot> diems = dataSnapshot.child("DanhSachHocSinh").child(hs.getKey())
+                                    .child("DanhSachDiem").getChildren().iterator();
+                            Iterator<DataSnapshot> hss = dataSnapshot.child("DanhSachHocSinh").child(hs.getKey()).getChildren().iterator();
+                            String tenhs = "";
+                            boolean hadten = false;
+                            while (hss.hasNext()){
+                                DataSnapshot hsitem = hss.next();
+                                if(hsitem.getKey().equals("Ten")){
+                                    tenhs = hsitem.getValue().toString();
+                                    hadten = true;
+                                }
+                            }
+                            if(!hadten)
+                                continue;
+                            boolean haddiem = false;
+                            while (diems.hasNext()) {
+                                DataSnapshot diem = diems.next();
+                                if (diem.getKey().equals(spinnerDanhSachDe.getSelectedItem().toString())) {
+                                    lisths.add(new UserInfo(tenhs, diem.getValue().toString()));
+                                    haddiem = true;
+                                    break;
+                                }
+                            }
+                            if (haddiem == false) {
+                                lisths.add(new UserInfo(tenhs, "Chưa làm bài."));
+                            }
+                        }
+//                        Message.showDialog(XemDiemActivity.this, lisths.size() + "");
+                        listView.setAdapter(new UserInfoAdapter(XemDiemActivity.this, R.layout.giaovien_itemdiem, lisths));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void addEvents() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void addControls() {
+        listView = findViewById(R.id.listView);
+        spinnerDanhSachLop = findViewById(R.id.spinnerDanhSachLop);
+        spinnerDanhSachDe = findViewById(R.id.spinnerDanhSachDe);
+        btnBack = findViewById(R.id.btnBack);
+
+        listde = new ArrayList<>();
+        listlop = new ArrayList<>();
+    }
+}
